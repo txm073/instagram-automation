f03fc38084885b99267ca53257a3b3cc0d54dd5e
Tom <tomtombarnes2@gmail.com>
2022-07-04 19:00:33 +0100

    Files updated at 04/07/2022 - 19:00

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 def runcmd(command):
     resp = requests.post("https://ig-api-proxy.herokuapp.com/service", data={"auth": "0b845a09dd890f71bd8f6ae99a74573f", "command": command})
     if resp.json().get("status", "error") == "success":
-        print(str(resp.json()["output"]))
+        print(str(resp.json()["stdout"]))
 
 command1 = "login:igapitest1,0x369CF"
 runcmd(command1)
